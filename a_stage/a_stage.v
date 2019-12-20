6edc247f3fcc183f12d05ce991f26a4b1761df0a
Ryo Nakagawa <funanori9292@gmail.com>
2019-12-20 17:55:38 +0900

    refactor a_stage

--- a_stage/a_stage.v
+++ a_stage/a_stage.v
@@ -1,45 +1,29 @@
-module a_stage(clk, rst,
-                v_i,
-                v_o,
-                data_i,
-                data_o,
-                stall_i,
-                stall_o
-                );
+module a_stage #(
+  parameter WORD = 32
+) (
+  input clk,
+  input rst,
+  input v_i,                    // valid in, if (v_i == 1) valid
+  output reg v_o,               // valid out, if (v_o == 1) valid 
+  input [WORD-1:0] data_i,      // data in
+  output reg [WORD-1:0] data_o, // data out
+  input stall_i,                // stall in, if (stall_i == 1) stall
+  output stall_o                // stall out, if (stall_o == 1) stall
+);
 
-    input           clk, rst;   //clock, reset
-    input           v_i;        // valid in, if (v_i == 1) valid
-    output          v_o;        // valid out, if (v_o == 1) valid
-    input [31:0]    data_i;     // data in
-    output [31:0]   data_o;     // data out
-    input           stall_i;    // stall in, if (stall_i == 1) stall
-    output          stall_o;    // stall out, if (stall_o == 1) stall
+  // stall to previous stage
+  assign stall_o = (v_o & stall_i);
 
-    // pipeline registers
-    reg         v_r;        // valid register for output
-    reg [31:0]  data_r;     // data register for output
-
-    // connecting registers to output
-    assign v_o = v_r;
-    assign data_o = data_r;
-
-    // stall to previous stage
-    assign stall_o = (v_r & stall_i);
-
-    always @(posedge clk or negedge rst)
-        begin
-            if (~rst) // reset
-                begin
-                    v_r <= 0;       // reset valid reg
-                    data_r <= 0;    // reset data reg
-                end
-            else
-                begin
-                    if (~stall_i) // valid and not stall
-                    begin // transfer to next stage
-                        v_r <= v_i;
-                        data_r <= data_i;
-                    end
-                end // else !if(~rst)
-        end // always @(posedge clk or negedge rst)
+  always @(posedge clk or negedge rst) begin
+    if (~rst) begin // reset
+      v_o <= 0;       // reset valid reg
+      data_o <= 0;    // reset data reg
+    end
+    else begin
+      if (~stall_i) begin // valid and not stall
+        v_o <= v_i;
+        data_o <= data_i;
+      end // if (~stall_i)
+    end // else !if(~rst)
+  end // always @(posedge clk or negedge rst)
 endmodule //a_stage
